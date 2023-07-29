f8c8648d98d969be5de1fbe6642b7f266f6707d4
LysovMaxim <max_maran@icloud.com>
2023-07-29 17:39:09 +0300

    added 4 routes and registered their work
    -register
    -login
    -current
    -logout

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+const { hendleMongooseError } = require("../helpers");
+const Joi = require("joi");
+
+const emailRegexp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+
+const userShema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+userShema.post("save", hendleMongooseError);
+
+const registerShema = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
+const loginShema = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
+const sсhemas = {
+  registerShema,
+  loginShema,
+};
+
+const User = model("user", userShema);
+
+module.exports = {
+  User,
+  sсhemas,
+};
