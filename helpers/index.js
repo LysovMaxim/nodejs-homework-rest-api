e8a75adcb79427414cc7df7c47b3be17501f25ff
LysovMaxim <max_maran@icloud.com>
2023-08-10 15:27:30 +0300

    made email confirmation

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,10 @@
 const HttpError = require("./HttpError");
 const hendleMongooseError = require("./hendleMongooseError");
+const sendEmail = require("./sendEmail");
+const createVerifyEmail = require("./createVerifyEmail");
 module.exports = {
   HttpError,
   hendleMongooseError,
+  sendEmail,
+  createVerifyEmail,
 };
